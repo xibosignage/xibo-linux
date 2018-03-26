f0335c3950ffbbd9e2424d74bcc714ef0cf6d276
Maxim Ivanov <maxim11853@gmail.com>
2018-03-26 15:38:44 +0300

    CommandLineParser added, logging level added to command line options

---
+++ Player/CommandLineParser.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <boost/program_options.hpp>
+#include <spdlog/spdlog.h>
+
+class CommandLineParser
+{
+public:
+    CommandLineParser(int argc, char** argv);
+    bool parse();
+
+    bool is_version() const;
+    bool is_example_dir() const;
+    bool is_testing() const;
+    std::string xlf_file() const;
+    std::string example_dir() const;
+    const boost::program_options::options_description& available_options() const;
+
+private:
+    std::string find_xlf_file(const std::string& example_dir_path);
+    bool check_example_dir(const std::string& example_dir);
+
+private:
+    int m_argc;
+    char** m_argv;
+    bool m_is_version = false;
+    bool m_is_example_dir = false;
+    bool m_is_testing = false;
+    std::string m_xlf_file;
+    std::string m_example_dir;
+    boost::program_options::options_description m_options;
+    std::shared_ptr<spdlog::logger> m_logger;
+};
--- Player/XiboApp.cpp
+++ Player/XiboApp.cpp
@@ -9,15 +9,11 @@
 #include "config.hpp"
 
 #include "XlfParser.hpp"
+#include "CommandLineParser.hpp"
 
 #include <iostream>
 #include <gstreamermm/init.h>
-#include <boost/filesystem.hpp>
-#include <boost/program_options.hpp>
-#include <spdlog/fmt/ostr.h>
 #include <glibmm/main.h>
-
-namespace fs = boost::filesystem;
 
 std::string XiboApp::s_example_dir;
 
@@ -25,41 +21,6 @@ XiboApp::XiboApp(const std::string& app_name)
 {
     init();
     m_app = Gtk::Application::create(app_name);
-}
-
-int XiboApp::run(int argc, char** argv)
-{
-    parse_command_line(argc, argv);
-    if(fs::exists(s_example_dir) && fs::is_directory(s_example_dir))
-    {
-        auto xlf_file_path = get_xlf_file();
-        if(!xlf_file_path.empty())
-        {
-            XlfParser parser(xlf_file_path);
-
-            auto layout = parser.parse_layout();
-            layout->show_regions();
-
-            m_logger->debug("Player started");
-
-            return m_app->run(*layout);
-        }
-        else
-        {
-            m_logger->error(".XLF file doesn't exist");
-        }
-    }
-    else if(!s_example_dir.empty())
-    {
-        m_logger->error("The directory doesn't exist (or it isn't a directory)");
-    }
-    return 0;
-}
-
-// temporary solution until receiving files from CMS
-const std::string& XiboApp::example_dir()
-{
-    return s_example_dir;
 }
 
 void XiboApp::init()
@@ -73,54 +34,40 @@ void XiboApp::init()
     m_logger = spdlog::get(LOGGER);
 }
 
-void XiboApp::parse_command_line(int argc, char **argv)
+int XiboApp::run(int argc, char** argv)
 {
-    namespace po = boost::program_options;
-
-    po::options_description desc("Allowed options");
-    desc.add_options()("example-dir", po::value<std::string>()->value_name("path-to-dir"), "specify full (absolute) path to example directory");
-    desc.add_options()("version", "get project version");
-    desc.add_options()("testing", "enable testing mode");
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-
-    if(!vm.empty())
+    CommandLineParser options(argc, argv);
+    bool result = options.parse();
+    if(result)
     {
-        if(vm.count("example-dir"))
-        {
-            s_example_dir = vm["example-dir"].as<std::string>();
-            m_logger->info("Example directory is {}", vm["example-dir"].as<std::string>());
-        }
-        if(vm.count("version"))
+        if(options.is_version())
         {
             m_logger->info("Project version: {}", get_version());
         }
-        if(vm.count("testing"))
+        if(options.is_example_dir())
         {
-            Glib::signal_timeout().connect_once([=](){
-                std::exit(0);
-            }, 3000);
+            if(options.is_testing())
+            {
+                Glib::signal_timeout().connect_once([=](){
+                    std::exit(0);
+                }, 3000);
+            }
+            s_example_dir = options.example_dir();
+
+            XlfParser parser(options.xlf_file());
+            auto layout = parser.parse_layout();
+            layout->show_regions();
+
+            m_logger->info("Player started");
+
+            return m_app->run(*layout);
         }
     }
-    else
-    {
-        m_logger->info("{}", desc);
-    }
+    return 0;
 }
 
-std::string XiboApp::get_xlf_file()
+// temporary solution until receiving files from CMS
+const std::string& XiboApp::example_dir()
 {
-    fs::directory_iterator it(s_example_dir);
-    fs::directory_iterator end;
-
-    while(it != end)
-    {
-        if(fs::is_regular_file(*it) && it->path().extension() == ".xlf")
-            return it->path().generic_string();
-        ++it;
-    }
-
-    return std::string{};
+    return s_example_dir;
 }
