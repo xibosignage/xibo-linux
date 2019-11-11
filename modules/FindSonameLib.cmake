1e899a70c293a8a25a3f7903c827a1b374cbb029
Maxim Ivanov <maxim11853@gmail.com>
2019-11-11 16:24:33 +0200

    build: update cmake lists to use best practices where possible

--- modules/FindSonameLib.cmake
+++
@@ -1,8 +0,0 @@
-function(find_soname_lib lib output_var)
-    execute_process(COMMAND objdump -p ${lib} COMMAND grep SONAME COMMAND xargs echo -n OUTPUT_VARIABLE soname_lib)
-    string(REPLACE "SONAME " "" soname_lib "${soname_lib}")
-
-    find_library(${lib}_soname_path NAMES ${soname_lib})
-
-    set(${output_var} ${${lib}_soname_path} PARENT_SCOPE)
-endfunction()
